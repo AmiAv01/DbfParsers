a597d85f8f772e66941d1fab290ce2cc85552276
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-06-07 14:41:45 +0300

    feat: add strategy pattern to choose parser

---
+++ src/Dbf/Parsers/ParserContext.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Dbf\Parsers;
+
+class ParserContext
+{
+    private AbstractDbfParser $parser;
+
+    public function __construct(AbstractDbfParser $parser)
+    {
+       $this->parser = $parser;
+    }
+
+    public function setParser(AbstractDbfParser $parser): void {
+        $this->parser = $parser;
+    }
+
+    public function execute(string $fileName, string $archivePath): void{
+        $startTime = microtime(true);
+        echo "Importing {$fileName} from {$archivePath}\n";
+        $this->parser->process();
+        echo "Import completed successfully!\n";
+        $endTime = microtime(true);
+        $executionTime = $endTime - $startTime;
+
+        echo "Время выполнения: " . round($executionTime, 4) . " секунд\n";
+    }
+}
